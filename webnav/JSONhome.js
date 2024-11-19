64df66d76997949f941b32efac9ce89864986080
wsj <sin@wsjdeiMac.local>
2024-11-19 09:40:27 +0800

    新增网址导航

---
+++ webnav/JSONhome.js
@@ -0,0 +1,504 @@
+
+var global_data = [
+  {
+    title: '常用',
+    data: [
+      {
+        link: "https://activity.lenovo.com.cn/club/lucky.html",
+        favicon: "",
+        text: "联想大转盘",
+        comment: "抽屉的13175164515-zxcvbnm5+1无大写联系邮箱415791939@qq.com;",
+      },
+      {
+        link: 'https://v2ex.com/',
+        favicon: '',
+        text: 'v2ex',
+        comment: '',
+      },
+      {
+        link: 'https://tophub.today/',
+        favicon: 'https://tophub.today/favicon.ico',
+        text: '今日热榜',
+        comment: '',
+      },
+      {
+        link: 'https://zhidao.baidu.com/shop/lottery',
+        favicon: '',
+        text: '百度乐透',
+        comment: '',
+      },
+      {
+        link: 'http://maolihui.com/',
+        favicon: './icon/maolihui.com.ico',
+        text: '猫理会',
+        comment: '',
+      },
+      {
+        link: 'http://www.jq22.com/',
+        favicon: './icon/jq22.ico',
+        text: 'jq22',
+        comment: '',
+      },
+      {
+        link: 'https://www.epicgames.com/store/zh-CN/',
+        favicon: 'https://static-assets-prod.epicgames.com/epic-store/static/favicon.ico',
+        text: 'Epic',
+        comment: '',
+      },
+      {
+        link: 'http://www.sucaihuo.com/Member/sign.html',
+        favicon: 'https://ossali.sucaihuo.com/sucaihuo/Public/images/favicon.ico',
+        text: '素材火',
+        comment: '',
+      },
+      {
+        link: 'http://vs.5eplay.com/cstrike',
+        favicon: '',
+        text: 'cs1.6服务器',
+        comment: '',
+      },
+      {
+        link: 'https://www.98zhibo.com/nbalx/',
+        favicon: '',
+        text: 'NBA回放',
+        comment: '',
+      },
+      {
+        link: 'https://www.yoozhibo.net/lanqiu/',
+        favicon: '',
+        text: '优直播',
+        comment: '',
+      },
+      {
+        link: 'https://www.chinataiwan.org/live/lanqiu/nba/',
+        favicon: '',
+        text: '极尽体育',
+        comment: '',
+      },
+      {
+        link: 'https://www.yehuowb.com/',
+        favicon: '',
+        text: '321体育',
+        comment: '',
+      },
+      {
+        link: 'https://xzb.live',
+        favicon: '',
+        text: '新直播',
+        comment: '',
+      },
+      {
+        link: 'https://www.956.tv/',
+        favicon: '',
+        text: '956体育',
+        comment: '',
+      },
+      {
+        link: 'http://www.popozhibo.xyz/match/1',
+        favicon: '',
+        text: '泡泡直播',
+        comment: '',
+      },
+      {
+        link: 'https://www.doozhibo.com/lanqiu/nba/',
+        favicon: '',
+        text: '豆直播',
+        comment: '',
+      },
+      {
+        link: 'https://tv.cctv.com/live/cctv5/?site=1',
+        favicon: '',
+        text: 'cctv5',
+        comment: ''
+      },
+      {
+        link: 'https://www.jytv600.com/live?id=63621393',
+        favicon: '',
+        text: '鲸鱼直播',
+        comment: ''
+      },
+      {
+        link: 'https://720pstream.nu/',
+        favicon: '',
+        text: '720pstream',
+        comment: ''
+      },
+      {
+        link: 'https://live.sporttuna.pro/',
+        favicon: '',
+        text: 'sporttuna',
+        comment: 'https://sporttuna.sx/live/mma/和https://sporttuna.sx/livestream/',
+      },
+      {
+        link: 'https://the.buffstream.io/mma-streams-live-10',
+        favicon: '',
+        text: 'buffstream',
+        comment: '',
+      },
+      {
+        link: 'https://byrut.org/',
+        favicon: '',
+        text: 'byrut',
+        comment: '',
+      },
+      {
+        link: 'https://www.skidrowreloaded.com/',
+        favicon: '',
+        text: 'skidrowreloaded',
+        comment: '',
+      },
+      {
+        link: 'https://rutracker.net/',
+        favicon: '',
+        text: 'rutracker',
+        comment: '账号vmzjekybrh密码@iubridge.com',
+      },
+      {
+        link: 'https://www.lmaoyx.com/',
+        favicon: '',
+        text: '老猫一修',
+        comment: '',
+      },
+      {
+        link: 'https://fitgirl-repacks.site/',
+        favicon: '',
+        text: 'fitgirl',
+        comment: 'ip地址109.94.209.70',
+      },
+      {
+        link: 'https://clarity.microsoft.com/projects',
+        favicon: '',
+        text: 'clarity',
+        comment: 'axlive.cn',
+      }
+    ],
+  },
+  {
+    title: '其他',
+    data: [
+      {
+        link: 'https://www.bet18lk.com/',
+        favicon: '',
+        text: '新利菠菜',
+        comment: 'axzx大m5+1邮箱csd@18luck.com',
+      },
+      {
+        link: 'https://www.google.com/adsense/v3/app?hl=zh_CN#home',
+        favicon: './icon/google.ico',
+        text: '谷歌联盟',
+        comment: '',
+      },
+
+      {
+        link: 'http://tongji.baidu.com/web/welcome/login',
+        favicon: './icon/baidutongji.png',
+        text: '百度统计',
+        comment: '',
+      },
+      {
+        link: 'http://dongtaiwang.com/loc/phome.php?v=0',
+        favicon: '',
+        text: '动态网',
+        comment: '',
+      },
+      {
+        link: 'https://www.smzdm.com/fenlei/jinrongchanpin/',
+        favicon: 'https://post.smzdm.com/favicon.ico',
+        text: '值得买羊毛',
+        comment: '',
+      },
+      {
+        link: 'https://www.yikm.net/nes?page=1&tag=8',
+        favicon: '',
+        text: '小霸王街机',
+        comment: '',
+      },
+      {
+        link: 'https://www.ali213.net/',
+        favicon: 'https://www.ali213.net/favicon.ico',
+        text: '游侠网',
+        comment: 'ax as4',
+      },
+      {
+        link: 'https://fuliba123.net/',
+        favicon: '',
+        text: '福导',
+        comment: '',
+      },
+
+      {
+        link: 'https://flingtrainer.com/',
+        favicon: '',
+        text: '游戏外挂',
+        comment: '',
+      },
+      {
+        link: 'https://patch.ali213.net/showclass/class1_1.html',
+        favicon: '',
+        text: '一修大师',
+        comment: '游戏外挂',
+      },
+      {
+        link: 'http://www.3h3.com/',
+        favicon: '',
+        text: '当游网',
+        comment: '',
+      },
+      {
+        link: 'https://aff.alimama.com/',
+        favicon: '',
+        text: '阿里妈妈',
+        comment: '',
+      }
+    ],
+  },
+  {
+    title: '下载',
+    data: [
+      {
+        link: 'https://www.gying.net/',
+        favicon: '',
+        text: 'pianku',
+        comment: 'miuqi miuqi002 全球:www.gying.net www.gying.org www.gying.si www.gying.in www.gyg.la www.gyg.si',
+      },
+      {
+        link: 'https://www.xl720.com/',
+        favicon: '',
+        text: '迅雷电影天堂',
+        comment: '',
+      },
+      {
+        link: 'http://ysxjjkl.souyisou.top/',
+        favicon: '',
+        text: '数据搜索',
+        comment: '',
+      },
+      {
+        link: 'https://www.clgod123.com/',
+        favicon: '',
+        text: '磁力天堂',
+        comment: ''
+      },
+
+      {
+        link: 'https://www.xinjuc.com',
+        favicon: '',
+        text: '新综艺',
+        comment: '',
+      },
+      {
+        link: 'http://www.dygangs.me/',
+        favicon: '',
+        text: '电影港',
+        comment: '',
+      },
+      {
+        link: 'https://www.z2pyw.com/',
+        favicon: '',
+        text: 'za片源网',
+        comment: '',
+      },
+
+
+
+
+      {
+        link: 'https://yzzy.tv/',
+        favicon: '',
+        text: '采集站',
+        comment: '主域名:yzzy.tv hdzyk.com(备用网址): 1080zyk1.com 至 1080zyk15.com',
+      },
+      {
+        link: 'https://www.dy2160.com/',
+        favicon: '',
+        text: 'dy2160',
+        comment: '',
+      },
+      {
+        link: 'https://www.rrdynb.com/index.html',
+        favicon: '',
+        text: '人人电影',
+        comment: '',
+      },
+
+      {
+        link: 'http://www.9bt0.com/',
+        favicon: '',
+        text: '不太灵',
+        comment: 'www.1bt0.com到www.9bt0.com 到2bt0',
+      },
+      {
+        link: 'http://www.66ys.co/',
+        favicon: '',
+        text: '66电视剧',
+        comment: '',
+      },
+
+      {
+        link: 'https://cupfox.app/',
+        favicon: 'https://i.loli.net/2018/08/04/5b65b28a65aae.png',
+        text: '咖啡狐',
+        comment: '',
+      },
+      {
+        link: 'https://www.993dy.com/',
+        favicon: 'https://www.993dy.com/favicon.ico',
+        text: '电影天堂',
+        comment: '',
+      },
+      {
+        link: 'https://subhd.tv/',
+        favicon: '',
+        text: '火星影视',
+        comment: '',
+      },
+      {
+        link: 'https://www.meijumi.net/',
+        favicon: '',
+        text: '美剧迷',
+        comment: '',
+      },
+      {
+        link: 'https://www.meijutt.tv/',
+        favicon: '',
+        text: '美剧天堂',
+        comment: '',
+      },
+      {
+        link: 'https://www.jianpian12.com/',
+        favicon: '',
+        text: '荐片播放器',
+        comment: 'www.jianpian9.com',
+      },
+      {
+        link: 'https://www.domp4.cc/',
+        favicon: '',
+        text: 'domp4',
+        comment: '',
+      },
+
+      {
+        link: 'https://subhd.tv/',
+        favicon: '',
+        text: 'subhd',
+        comment: '',
+      },
+
+      {
+        link: 'https://www.ciligod.com/',
+        favicon: '',
+        text: '磁力天堂',
+        comment: '',
+      },
+      {
+        link: 'https://www.yxdju.cc/',
+        favicon: '',
+        text: '一休短剧',
+        comment: '',
+      }
+
+    ],
+  },
+  {
+    title: '在线',
+    data: [
+      {
+        link: 'https://yingheapp.com/',
+        favicon: '',
+        text: '硬核指南',
+        comment: '备用域名https://硬核指南.com 失联yingheapp#hotmail.com',
+      },
+      {
+        link: 'https://lkvod.me/',
+        favicon: '',
+        text: '来看点播',
+        comment: '',
+      },
+      {
+        link: 'https://www.hdmoli.pro/',
+        favicon: '',
+        text: 'hdmoli',
+        comment: '',
+      },
+      {
+        link: 'https://pianyuan.org/',
+        favicon: '',
+        text: 'pianyuan',
+        comment: 'miuqi2@163 as4',
+      },
+      {
+        link: 'https://www.ysgc.fun/',
+        favicon: '',
+        text: '影视工厂',
+        comment: '备用https://ysgcapp.com/ http://3443.tv',
+      },
+      {
+        link: 'https://ddys.tv',
+        favicon: '',
+        text: 'ddrk',
+        comment: '',
+      },
+      {
+        link: 'http://www.loldytt.org/',
+        favicon: '',
+        text: '电影天堂',
+        comment: '',
+      },
+      {
+        link: 'https://www.lgyy.vip/',
+        favicon: '',
+        text: '蓝光影院',
+        comment: '备用www.lgyy.vip和6446.tv',
+      },
+      {
+        link: 'http://klyingshi.vip/',
+        favicon: '',
+        text: 'coke',
+        comment: '收藏发布页：http://klyingshi.vip,综合导航：https://36kdh.com,关注公众号『可乐影视圈』可获取最新地址',
+      },
+
+      {
+        link: 'https://klyingshi.com/',
+        favicon: '',
+        text: '可乐影视大全',
+        comment: '备用http://klyingshi.vip/',
+      },
+      {
+        link: 'https://www.libvio.cc/play/714889315-2-15.html',
+        favicon: '',
+        text: '终末武神',
+        comment: '失联libvio@outlook.com',
+      },
+    ],
+  },
+  {
+    title: '游戏直播',
+    data: [
+      {
+        link: 'https://www.douyu.com/255329',
+        favicon: './icon/douyu.png',
+        text: 'fly100%',
+        comment: '',
+      },
+      {
+        link: 'https://www.douyu.com/1786555',
+        favicon: './icon/douyu.png',
+        text: '120',
+        comment: '',
+      },
+      {
+        link: 'http://www.huya.com/Yumiko',
+        favicon: 'https://www.huya.com/favicon.ico',
+        text: 'yumiko',
+        comment: '',
+      },
+      {
+        link: 'https://space.bilibili.com/470156882/',
+        favicon: 'https://www.bilibili.com/favicon.ico',
+        text: '哔哩哔哩',
+        comment: '',
+      },
+    ],
+  },
+]
+
---
+++ webnav/handlehome.js
@@ -0,0 +1,65 @@
+
+document.getElementById('ipt_text').onkeydown = function (e) {
+  // if(e.ctrlKey && e.keyCode===13){
+  //     window.location.href="https://xilesou.hk.gg363.site/search?q="+this.value
+  // }
+  if (e.keyCode === 13) {
+    window.open("http://baidu.com/s?wd=" + this.value, '_blank')
+  }
+}
+document.getElementById('btn_baidu').onclick = function (e) {
+  var _val = document.getElementById('ipt_text').value
+  if (_val) window.open("http://baidu.com/s?wd=" + _val, '_blank');
+  else window.open("http://baidu.com/", '_blank');
+}
+
+document.getElementById('btn_bing').onclick = function (e) {
+  var _val = document.getElementById('ipt_text').value
+  if (_val) window.open("https://cn.bing.com/search?q=" + _val, '_blank');
+  else window.open("https://cn.bing.com", '_blank');
+}
+async function getCurrentTab() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) {
+        var tabId = tabs[0].id;
+        resolve(tabId)
+      }
+    });
+  })
+
+}
+document.getElementById('gopage').onclick = async () => {
+  var TABID = await getCurrentTab()
+  chrome.tabs.create({ "url": "webnav/work.html" });
+  chrome.tabs.remove(TABID);
+}
+
+
+function tplSection(data) {
+  var ss = ''
+  data.data.forEach((v, k) => {
+    let { link, comment, favicon, text } = v
+    if (favicon) {
+      favicon = `<img src="${favicon}" alt="" class="link-item-favicon">`
+    } else {
+      favicon = `<div class="link-item-nofav"></div>`
+    }
+    ss += `<a href="${link}" title="${comment}" class="link-item">${favicon}${text}</a>`
+  })
+  return (
+    `<div class="box-wrap">
+        <div class="panel-head">${data.title}</div>
+        <div class="panel-body">${ss}</div>
+    </div>`
+  )
+}
+
+function render(data) {
+  var cc = ''
+  data.forEach((v, k) => {
+    cc += tplSection(v)
+  })
+  document.getElementById('ctx').innerHTML = cc
+}
+render(global_data)
